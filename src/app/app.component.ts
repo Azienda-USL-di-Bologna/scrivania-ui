2c28779a693cecb292d5495614bf254b6995a574
l.salomone <devnull@localhost>
2018-11-29 13:39:24 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NtJwtLoginService } from '@bds/nt-jwt-login';
+import { getInternautaUrl, BaseUrlType } from 'src/environments/app-constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Babel-internauta';
 
+  constructor(private loginService: NtJwtLoginService) {}
 
+  ngOnInit() {
+    this.loginService.setloginUrl(getInternautaUrl(BaseUrlType.Login));
+  }
 
 
 }
