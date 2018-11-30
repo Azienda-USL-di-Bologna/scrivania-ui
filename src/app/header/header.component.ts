59b742af160f5da6928512377ac3c238d9b1ca99
l.salomone <devnull@localhost>
2018-11-30 11:16:10 +0100

    cambio utente

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -55,6 +55,6 @@ export class HeaderComponent implements OnInit {
   onCambioUtente(persona: Persona) {
     this.cambioUtentePopupVisibile = false;
 
-    window.open(this.router.serializeUrl(this.router.createUrlTree(['/'], { queryParams: { 'impersonatedUser': persona.codiceFiscale } })));
+    window.open(this.router.serializeUrl(this.router.createUrlTree(['.'], { queryParams: { 'impersonatedUser': persona.codiceFiscale } })));
   }
 }
