7316f72ad0e2476c512790e12362242716aba371
a.marcomini <devnull@localhost>
2018-12-11 10:57:18 +0100

    fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -54,15 +54,15 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  onCambioUtente(persona: Persona) {
+  onCambioUtente(utente: Utente) {
     console.log("header onCambioUtente")
     this.cambioUtentePopupVisibile = false;
 
     let url: string = '';
     if (window.location.href.indexOf('?') >= 0)
-      url = window.location.href.toString() + '&impersonatedUser=' + persona.codiceFiscale;
+      url = window.location.href.toString() + '&impersonatedUser=' + utente.idPersona.codiceFiscale;
     else
-      url = window.location.href.toString() + '?impersonatedUser=' + persona.codiceFiscale;
+      url = window.location.href.toString() + '?impersonatedUser=' + utente.idPersona.codiceFiscale;
 
 
      window.open(url, '_blank');
