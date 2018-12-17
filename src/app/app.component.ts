ebfc353247711d4c312ae40facbededfe2ce065e
a.marcomini <devnull@localhost>
2018-12-17 10:31:48 +0100

    prova cambia utente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements OnInit {
     this.route.queryParams.subscribe((params: Params) => {
       console.log("dentro subscribe, ", params.hasOwnProperty('impersonatedUser'));
       console.log("chiamo login");
-      console.log(params['impersonatedUser']);
+      console.log("impersonateUser: ", params['impersonatedUser']);
 
       // se nei params c'è la proprietà impersonatedUser, allora pulisci la sessione, setta nella sessionStorage l'utente impersonato
       // e cancellalo dai params
@@ -44,11 +44,12 @@ export class AppComponent implements OnInit {
         this.loginService.clearSession();
         this.loginService.setimpersonatedUser(params['impersonatedUser']);
         //sessionStorage.setItem('impersonatedUser', params['impersonatedUser']);
+        window.history.replaceState("object or string", "Title", window.location.pathname.split("?")[0]);
         delete params['impersonatedUser'];
         
         //window.history.replaceState("object or string", "Title", window.location.pathname.split("?")[0]);
       }
-      window.history.replaceState("object or string", "Title", window.location.pathname.split("?")[0]);
+      
       //this.ntJwtLoginComponent.doLogin();
    });
   }
