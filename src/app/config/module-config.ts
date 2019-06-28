f89554701410ea441e311050228ddd0660f808e1
g.demarco <devnull@localhost>
2019-06-28 12:42:08 +0200

    disattivato logout automatico

--- src/app/config/module-config.ts
+++ src/app/config/module-config.ts
@@ -8,5 +8,5 @@ export const loginModuleConfig: NTJWTModuleConfig = {
     localhostPort: LOCALHOST_PORT,
     applicazione: APPLICATION,
     logoutRedirectRoute: SCRIVANIA_ROUTE,
-    sessionExpireSeconds: 1800
+    sessionExpireSeconds: 0 // 0 = distattivato
 };
